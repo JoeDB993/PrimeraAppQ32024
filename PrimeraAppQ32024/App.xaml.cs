3abeb22f25cd18d74266e832777a19f4a51949fa
JoeDB993 <131511247+JoeDB993@users.noreply.github.com>
2024-08-14 19:47:00 -0600

    Agregar archivos de proyecto.

---
+++ PrimeraAppQ32024/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace PrimeraAppQ32024
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ PrimeraAppQ32024/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace PrimeraAppQ32024
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
